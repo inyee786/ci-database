8c528e0f812a2f0ed2e1656b796807c5115a97bd
Chandan Kumar <chandan.kumar@mayadata.io>
2018-11-26 13:14:33 +0530

    Add pipeline data for aws and gke
    - Created two table in database for aws and gke.
    - Fetch aws and gke pipeline data from gitlab and dump into database
    - Crated two routes path one is /aws and other is /gke
    
    Signed-off-by: Chandan Kumar <chandan.kumar@mayadata.io>

--- main.go
+++ main.go
@@ -45,9 +45,54 @@ type dashboard struct {
 	Dashboard []dashboardSummary
 }
 
-func main() {
+type dashboardaws struct {
+	Dashboardaws []dashboardSummary
+}
 
-	// Get data from gitlab api
+func main() {
+	initDb()
+	defer db.Close()
+	http.HandleFunc("/gke", gkedata)
+	http.HandleFunc("/aws", awsdata)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func gkedata(w http.ResponseWriter, r *http.Request) {
+	datas := dashboard{}
+	err := queryData(&datas)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	out, err := json.Marshal(datas)
+	// log.Infof("%+v", string(out))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	fmt.Fprintf(w, string(out))
+	go gkeData()
+}
+
+func awsdata(w http.ResponseWriter, r *http.Request) {
+	datasaws := dashboardaws{}
+	err := queryAwsData(&datasaws)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	outaws, err := json.Marshal(datasaws)
+	// log.Infof("%+v", string(out))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	fmt.Fprintf(w, string(outaws))
+	go awsData()
+}
+
+// Get data from gitlab api for gke and push to database
+func gkeData() {
 	url := "https://gitlab.openebs.ci/api/v4/projects/24/pipelines?ref=master"
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("PRIVATE-TOKEN", "GN5eUuyg-ybHErwYLR3T")
@@ -60,55 +105,88 @@ func main() {
 	// fmt.Println(string(body))
 	var y Pipeline
 	json.Unmarshal(body, &y)
-	// log.Infof("%+v", y)
-	// api request end
-
-	initDb()
-	defer db.Close()
-
-	// --------------
 	for i := range y {
-		// log.Infof("%+v", y[i])
 		sqlStatement := `
-		INSERT INTO aws (id, sha, ref, status, web_url)
-		VALUES ($1, $2, $3, $4, $5)
-		ON CONFLICT (id) DO UPDATE
-		SET status = $4
-		RETURNING id`
+			INSERT INTO gke (id, sha, ref, status, web_url)
+			VALUES ($1, $2, $3, $4, $5)
+			ON CONFLICT (id) DO UPDATE
+			SET status = $4
+			RETURNING id`
 		id := 0
 		err = db.QueryRow(sqlStatement, y[i].ID, y[i].Sha, y[i].Ref, y[i].Status, y[i].WebURL).Scan(&id)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("New record ID is:", id)
+		fmt.Println("New record ID for gke:", id)
 	}
-	// --------------
-
-	http.HandleFunc("/api", indexHandler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	datas := dashboard{}
-
-	err := queryData(&datas)
+// Get data from gitlab api for aws and push to database
+func awsData() {
+	url := "https://gitlab.openebs.ci/api/v4/projects/7/pipelines?ref=master"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("PRIVATE-TOKEN", "GN5eUuyg-ybHErwYLR3T")
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
 		return
 	}
-
-	out, err := json.Marshal(datas)
-	// log.Infof("%+v", string(out))
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println(string(body))
+	var y Pipeline
+	json.Unmarshal(body, &y)
+	for i := range y {
+		sqlStatement := `
+			INSERT INTO aws (id, sha, ref, status, web_url)
+			VALUES ($1, $2, $3, $4, $5)
+			ON CONFLICT (id) DO UPDATE
+			SET status = $4
+			RETURNING id`
+		id := 0
+		err = db.QueryRow(sqlStatement, y[i].ID, y[i].Sha, y[i].Ref, y[i].Status, y[i].WebURL).Scan(&id)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("New record ID for aws:", id)
 	}
-
-	fmt.Fprintf(w, string(out))
 }
 
 // queryData first fetches the dashboard data from the db
 func queryData(datas *dashboard) error {
+	rows, err := db.Query(`
+		SELECT
+			*
+		FROM gke
+		ORDER BY id DESC`)
+	// log.Infof("%+v", rows)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		data := dashboardSummary{}
+		err = rows.Scan(
+			&data.ID,
+			&data.Sha,
+			&data.Ref,
+			&data.Status,
+			&data.WebURL,
+		)
+		if err != nil {
+			return err
+		}
+		// log.Infof("%+v", data)
+		datas.Dashboard = append(datas.Dashboard, data)
+	}
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// queryData first fetches the dashboard data from the db
+func queryAwsData(datasaws *dashboardaws) error {
 	rows, err := db.Query(`
 		SELECT
 			*
@@ -131,7 +209,8 @@ func queryData(datas *dashboard) error {
 		if err != nil {
 			return err
 		}
-		datas.Dashboard = append(datas.Dashboard, data)
+		// log.Infof("%+v", data)
+		datasaws.Dashboardaws = append(datasaws.Dashboardaws, data)
 	}
 	err = rows.Err()
 	if err != nil {
@@ -156,7 +235,7 @@ func initDb() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Successfully connected!")
+	fmt.Println("Successfully connected to Database!")
 }
 
 func dbConfig() map[string]string {
